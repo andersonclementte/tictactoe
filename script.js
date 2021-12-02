921901d2efcc5f2660bd92f2905c4c69c5bd62b5
andersonclementte <anderson.clementte@gmail.com>
2021-12-02 14:38:39 -0300

    algoritmo minmax que esqueci de commitar

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ function emptySquares(){
 }
 
 function bestSpot(){
-    return emptySquares()[0];
+    return minmax(origBoard, aiPlayer).index;
 }
 
 function checkTie(){
@@ -85,4 +85,53 @@ function checkTie(){
         return true;
     }
     return false;
+}
+
+function minmax(newBoard, player) {
+    var availSpots = emptySquares();
+    if (checkWin(newBoard, huPlayer)){
+        return {score: -10};
+    }
+    else if (checkWin(newBoard, aiPlayer)){
+        return {score: 10};
+    }
+    else if (availSpots.length === 0){
+        return {score: 0};
+    }
+    var moves = [];
+    for (var i = 0; i < availSpots.length; i++){
+        var move = {};
+        move.index = newBoard[availSpots[i]];
+        newBoard[availSpots[i]] = player;
+        if (player == aiPlayer){
+            var result = minmax(newBoard, huPlayer);
+            move.score = result.score;
+        }
+        else {
+            var result = minmax(newBoard, aiPlayer);
+            move.score = result.score;
+        }
+        newBoard[availSpots[i]] = move.index;
+        moves.push(move);
+    }
+    var bestMove;
+    if (player === aiPlayer){
+        var bestScore = -10000;
+        for (var i = 0; i < moves.length; i++){
+            if (moves[i].score > bestScore){
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    }
+    else {
+        var bestScore = 1000;
+        for (var i = 0; i < moves.length; i++){
+            if (moves[i].score < bestScore){
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    }
+    return moves[bestMove];
 }
